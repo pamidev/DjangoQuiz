4e968fd30db1cb5b6083d4efcda5501d114c8e7a
pamidev <pamidev@duck.com>
2023-03-01 21:00:18 +0100

    Added polls app to INSTALLED_APPS

--- djangoquiz/settings.py
+++ djangoquiz/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-y^!3o@jdi$)*@lz_qvhgo+)1a!ro_w)eiqpj2@4zn-bpn^@s56
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
@@ -104,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pl-pl'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Warsaw'
 
 USE_I18N = True
 
