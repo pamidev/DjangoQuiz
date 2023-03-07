841b45e7b3ad238a7f87f7a83c7fb7aa89255426
pamidev <pamidev@duck.com>
2023-03-07 20:23:29 +0100

    Added protection against displaying future questions

--- polls/views.py
+++ polls/views.py
@@ -1,10 +1,9 @@
 from django.http import HttpResponseRedirect
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import render, get_object_or_404
+from .models import Question, Choice
 from django.urls import reverse
-from django.utils import timezone
 from django.views import generic
-
-from .models import Choice, Question
+from django.utils import timezone
 
 
 class IndexView(generic.ListView):
@@ -25,6 +24,12 @@ class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
 
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
+
 
 class ResultsView(generic.DetailView):
     model = Question
@@ -36,6 +41,7 @@ def vote(request, question_id):
     try:
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
         return render(request, 'polls/detail.html', {
             'question': question,
             'error_message': "You didn't select a choice.",
@@ -43,4 +49,7 @@ def vote(request, question_id):
     else:
         selected_choice.votes += 1
         selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
